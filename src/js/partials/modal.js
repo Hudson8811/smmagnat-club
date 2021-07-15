5329ec7710d63a05a60447d489659c007c1ad87c
burdms <burdms@gmail.com>
2021-07-15 03:47:26 +0200

    fix: blur when modal is opened

--- src/js/partials/modal.js
+++ src/js/partials/modal.js
@@ -1,4 +1,5 @@
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.modal'),
+  mainContent = document.querySelector('.content');
 
 modal.addEventListener('click', event => {
   const target = event.target;
@@ -6,5 +7,13 @@ modal.addEventListener('click', event => {
   if (target.classList.contains('modal__overlay')) {
     modal.classList.remove('active');
     document.body.classList.remove('js-no-scroll');
+    mainContent.classList.remove('blur');
   }
 });
+
+/*
+  Когда модалка открыта, то 
+  - у .modal добавляется класс .active,
+  - у body добавляется класс js-no-scroll,
+  - у .content добавляется класс blur
+*/
